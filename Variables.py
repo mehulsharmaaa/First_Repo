d549376d159987ae9e2facf9c7104890c00b25b6
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-25 13:45:04 +0700

    CP2

--- Variables.py
+++ Variables.py
@@ -12,3 +12,22 @@ FIRSTNAME='JACK'
 LASTNAME='RYAN'
 FULLNAME=FIRSTNAME +" "+ LASTNAME
 print(FULLNAME)
+
+#TYPES:
+x=1
+#finding type of x
+print(type(x))
+#x is a intger
+
+y=2.1
+print(type(y))
+#y is a float
+
+z=2+7j
+print(type(z))
+#z is a complex
+
+X='GG'
+print(type(X))
+#X is a string
+
