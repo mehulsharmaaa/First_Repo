c26ab6d0feb006f2b8f43ac0300e127e801f8670
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-26 14:01:34 +0700

    CP7

---
+++ Basic operators.py
@@ -0,0 +1,23 @@
+#SOME BASIC OPERATORS:
+
+#ARITHEMATIC OPERATORS:
+#EXPONENT:
+x,y = 10,3 ;
+print(x**y)
+
+#FLOOR DIVISION:
+print(x//y)
+
+#ASSIGNMENT OPERATORS:
+a = 3
+b = 4
+c = a//b
+c += 1 #Basically means c = c+1
+c -= 1 #Basically means c = c-1
+c *= 1 #Basically means c = c*1
+c /= 1 #Basically means c = c/1
+c %= 1 #Basically means c = c%1
+c **= 1 #Basically means c = c**1
+c //= 1 #Basically means c = c//1
+
+#COMPARISON OPERATORS FURTHER
