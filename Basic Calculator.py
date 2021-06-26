9602f5fa636fee918d625945ac89b5730d7d051c
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-26 13:40:00 +0700

    CP6

---
+++ Basic Calculator.py
@@ -0,0 +1,13 @@
+#CREATING MOST BASIC CALCULATOR:
+x = float(input('ENTER FIRST NUMBER: '))
+y = float(input('ENTER SECOND NYMBER: '))
+#ADDITION:
+print('The sum is' + ' ' + str(x+y))
+#SUBTRACTION:
+print('The difference is' + ' ' + str(x-y))
+#MULTIPLICATION:
+print('The product is' + ' ' + str(x*y))
+#DIVISION:
+print('The result of division is' + ' ' + str(x/y))
+#MODULUS:
+print('The modulus is' + ' ' + str(x%y))
