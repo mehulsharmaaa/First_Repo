642173c4bc2ca733eaccf47d406a7853d3838222
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-26 13:20:54 +0700

    CP5

---
+++ Input.py
@@ -0,0 +1,10 @@
+#GETTING INPUT FROM A USER:
+Name = input('ENTER NAME: ')
+print('Hello' + ' ' + Name)
+
+#SPECIFYING THE DATA TYPE OF THE INPUT:
+Name = str(input('ENTER YOUR NAME: '))
+print('WELCOME' + ' ' + Name)
+
+Age = int(input('ENTER YOUR AGE: '))
+print(Age)
