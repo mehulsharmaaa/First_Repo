e330ba5ad9a94481962f4a9b3c9d5d525c727a92
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-28 23:57:12 +0700

    CP13

--- Tuple operations.py
+++ Tuple operations.py
@@ -4,5 +4,5 @@ if 'mango' in T: #Checks whether the element is available in tuple
     print('yes')
 print(len(T)) #Prints number of elements
 print(T.index('apple')) #Prints index of the element
-del T #Deletes the tuple
+del T #Deletes the whole tuple
 print(T)
