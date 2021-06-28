0e84215a84df535dd03c77278e317da6f3a194db
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-29 00:10:26 +0700

    CP14

---
+++ Set operations.py
@@ -0,0 +1,7 @@
+#SET OPERATIONS:
+
+S = {'apples', 'mangoes'}
+T = {'bananas', 'oranges', 'apples' }
+print(S.union(T)) #Combines two sets
+print(S.intersection(T)) #Prints common elements
+print(S.difference(T)) #Removes the elements that are common in both sets from the first
