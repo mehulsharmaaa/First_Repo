546d149b321558dd291af953c2590ffb4103230d
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-29 14:41:23 +0700

    CP12

--- List operations.py
+++ List operations.py
@@ -6,6 +6,14 @@ print(len(L)) #Number of elements inside list
 L.append('Microsoft') #adds element to the list
 L.insert(0, 'Sony') #adds element at desired index
 L.remove('Sony') #removes element
+L.pop(1) #removes element on the basis of index number
+L.replace(0) = 'cherries' #replaces element
 del L[0] #deletes element at desired index
-L.clear()
+L.clear() #clears all the elements form the list
 print(L)
+
+M = [1,3,2]
+M.sort() #arranges the elements
+print(L + M) #combines two lists
+print(max(M)) #gives maximum value
+print(min(M)) #gives minimum value
