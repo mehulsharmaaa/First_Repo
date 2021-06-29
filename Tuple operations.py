17797dabee50bb309f650a4669d6fd6864b0d908
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-29 14:48:47 +0700

    CP13

--- Tuple operations.py
+++ Tuple operations.py
@@ -6,3 +6,12 @@ print(len(T)) #Prints number of elements
 print(T.index('apple')) #Prints index of the element
 del T #Deletes the whole tuple
 print(T)
+
+#UNPACKING TUPLES:
+ABC = ('I', 'AM', 'FREAK')
+print(A)
+print(B)
+print(C)
+
+#NESTING TUPLES: Combining two tuples but each tuple retain its identity
+print(T + ABC)
