282fb9e53e9b0a227805ad43b36a778fac1d47b0
mehulsharmaaa <iammhlsha@gmail.com>
2021-06-30 15:35:28 +0700

    CP15

---
+++ For loop.py
@@ -0,0 +1,11 @@
+#FOR LOOP:
+
+L = ['appple', 'mangoes', 'bananas']
+for items in L: #Used for iterating over a sequence
+    print(items)
+
+M = 'Example'
+for word in M: #For loop in string
+    print(word)
+
+print(items) #If we know print iterator from the first example it'll print the last element from the sequence as it was stored as a value lastly
---
+++ Range.py
@@ -0,0 +1,12 @@
+#RANGE FUNCTION:
+
+for x in range(9): #Range basically specifies returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number
+    print(x)
+for y in range(2,5): #Starting point is specified too
+    print(y)
+for z in range(2,20,2): #Starting, end and increment is specified
+    print(z)
+
+L = ['appple', 'mangoes', 'bananas', 'oranges', 'cherries']
+for items in range(1,3): 
+    print(L[items])
